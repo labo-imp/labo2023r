39f7c56c7856859983a3bc30970267cf2d54b762
“MarceloTisera” <arielmarcelotisera@gmail.com>
2023-07-07 02:50:48 +0000

    experimento colaborativo

--- src/workflow-inicial/e641_TS_training_strategy.r
+++ src/workflow-inicial/e641_TS_training_strategy.r
@@ -11,34 +11,31 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "TS6410"
+PARAM$experimento <- "TS9410"
 
-PARAM$exp_input <- "FE6310"
+PARAM$exp_input <- "FE9310"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
 
 PARAM$future <- c(202109)
 PARAM$final_train <- c(
-  202107, 202106, 202105, 202104, 202103, 202102,
-  202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912,
-  201911, 201910, 201909
+  201912, 202001, 202002, 202011, 202012,
+  202101, 202102, 202103, 202104, 202105, 202106, 202107
 )
 
 PARAM$train$training <- c(
-  202105, 202104, 202103, 202102, 202101,
-  202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911,
-  201910, 201909, 201908, 201907
+ 201907, 201908, 201909, 201910, 201911, 201912, 202001, 202002, 202011, 202012, 202101, 202102
 )
 
-PARAM$train$validation <- c(202106)
-PARAM$train$testing <- c(202107)
+PARAM$train$validation <- c(202104)
+PARAM$train$testing <- c(202107) # solo testeo con un solo mes
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
 PARAM$train$undersampling <- 0.1
 
-PARAM$train$semilla <- 102191 # cambiar por su propia semilla  !!!
+PARAM$train$semilla <- 509067 # cambiar por su propia semilla  !!!
 
 PARAM$home <- "~/buckets/b1/"
 # FIN Parametros del script
