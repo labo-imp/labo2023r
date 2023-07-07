55f619a4f41814e84641661c8634d052fb429c48
“MarceloTisera” <arielmarcelotisera@gmail.com>
2023-07-07 02:35:41 +0000

    experimento colaborativo

--- src/workflow-inicial/e621_DR_corregir_drifting.r
+++ src/workflow-inicial/e621_DR_corregir_drifting.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "DR6210"
+PARAM$experimento <- "DR9210"
 
-PARAM$exp_input <- "CA6110"
+PARAM$exp_input <- "CA9110"
 
 PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
