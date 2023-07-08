9ec1ed5b62e56a0a5dc19fc27e1eb33dff337e23
“MarceloTisera” <arielmarcelotisera@gmail.com>
2023-07-08 13:32:57 +0000

    experimentos colaborativos base

--- src/workflow-inicial/e601_RUN_correr_workflow.r
+++ src/workflow-inicial/e601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/z611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/z621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/z631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/z641_TS_training_strategy.r")
+source("~/labo2023r/src/workflow-inicial/e611_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-inicial/e621_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-inicial/e631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/e641_TS_training_strategy.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/z651_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-inicial/z661_ZZ_final.r")
+source("~/labo2023r/src/workflow-inicial/e651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/e661_ZZ_final.r")
