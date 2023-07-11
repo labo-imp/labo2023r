ee370b7dbf70d46c4089567cf5dd7b8c93e85c84
AhumadaJulieta <127358388+AhumadaJulieta@users.noreply.github.com>
2023-07-11 15:54:42 -0300

    Add files via upload

---
+++ src/workflow-inicial/A_25_601_RUN_correr_workflow.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023r/src/workflow-inicial/A_25_611_CA_reparar_dataset.r")
+source("~/labo2023r/src/workflow-inicial/A_25_621_DR_corregir_drifting.r")
+source("~/labo2023r/src/workflow-inicial/A_25_631_FE_historia.r")
+source("~/labo2023r/src/workflow-inicial/A_25_641_TS_training_strategy.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023r/src/workflow-inicial/A_25_651_HT_lightgbm.r")
+source("~/labo2023r/src/workflow-inicial/A_25_661_ZZ_final.r")
